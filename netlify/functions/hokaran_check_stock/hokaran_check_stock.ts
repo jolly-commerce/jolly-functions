3f29670cf6a31108970ef6c19aa1c54eb16405fc
Kevin Miguet <kevin@Kevins-MacBook-Pro.local>
2023-05-30 16:01:06 +0400

    check hokaran stock

---
+++ netlify/functions/hokaran_check_stock/hokaran_check_stock.ts
@@ -0,0 +1,133 @@
+import { Handler } from "@netlify/functions";
+import * as https from "https";
+import { Type } from "typescript";
+
+export const handler: Handler = async (event, context) => {
+  const SHOPIFY_ACCESS_TOKEN = process.env.HOKARAN_SHOPIFY_ACCESS_TOKEN;
+  const LOCATION_ID = "gid://shopify/Location/5907972207";
+
+  const shopifyGraphEndpoint =
+    "https://hokaran.myshopify.com/admin/api/2023-04/graphql.json";
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ err: "no body :( " }),
+    };
+  }
+  const body = JSON.parse(event.body);
+
+  const queryBody = `
+  {
+    productVariant(id: "gid://shopify/ProductVariant/${body.variant_id}") {
+      id
+      title
+      inventoryItem {
+        id
+        inventoryLevels(first: 10) {
+          edges {
+            node {
+              available
+              location {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  `;
+
+  const shopifyData: any = await makeRequest(
+    shopifyGraphEndpoint,
+    "POST",
+    {
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": SHOPIFY_ACCESS_TOKEN, // Replace with your actual access token
+    },
+    JSON.stringify({ query: queryBody })
+  );
+
+  // Handle the GraphQL response
+  if (!shopifyData || !shopifyData.data || !shopifyData.data.productVariant) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: "Invalid response from Shopify" }),
+    };
+  }
+
+  const variant = shopifyData.data.productVariant;
+  const inventoryItem = variant.inventoryItem;
+
+  if (
+    !inventoryItem ||
+    !inventoryItem.inventoryLevels ||
+    !inventoryItem.inventoryLevels.edges
+  ) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: "No inventory data available" }),
+    };
+  }
+
+  let stockAvailable = null;
+
+  for (let edge of inventoryItem.inventoryLevels.edges) {
+    const node = edge.node;
+    if (node.location && node.location.id === LOCATION_ID) {
+      stockAvailable = node.quantities.find(
+        (q) => q.name === "available"
+      )?.quantity;
+      break;
+    }
+  }
+
+  if (stockAvailable === null) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        err: "No stock data available for the specified location",
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ stock: stockAvailable }),
+  };
+};
+
+function makeRequest(url, method, headers = {}, body?: any) {
+  return new Promise((resolve, reject) => {
+    const parsedUrl = new URL(url);
+    const options = {
+      hostname: parsedUrl.hostname,
+      path: parsedUrl.pathname + parsedUrl.search,
+      method: method,
+      headers: headers,
+    };
+
+    const req = https.request(options, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        resolve(JSON.parse(data));
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+    });
+
+    if (body) {
+      req.write(body);
+    }
+
+    req.end();
+  });
+}
