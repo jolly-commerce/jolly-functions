00e10cd36564e3778122120ced5c8e8555766e6b
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-12-13 19:31:29 +0200

    feat: function to parse data from current Variant

--- netlify/functions/hokaran_stock_tag_update/hokaran_stock_tag_update.ts
+++ netlify/functions/hokaran_stock_tag_update/hokaran_stock_tag_update.ts
@@ -1,8 +1,11 @@
+// to run : npm i, npm install netlify-cli -g, npx netlify dev
+// webhook on shopify store "Inventory item update" send to https://lucky-teeth-fry.loca.lt/.netlify/functions/hokaran_stock_tag_update
+
 import { Handler } from "@netlify/functions";
 import * as https from "https";
 
-
 const SHOPIFY_ACCESS_TOKEN = process.env.HOKARAN_SHOPIFY_ACCESS_TOKEN;
+const shopifyGraphEndpoint = "https://hokaran.myshopify.com/admin/api/2023-04/graphql.json";
 
 export const handler: Handler = async (event, context) => {
   const CORS_HEADERS = {
@@ -24,16 +27,8 @@ export const handler: Handler = async (event, context) => {
   //
   const shopifyGraphEndpoint =
     "https://hokaran.myshopify.com/admin/api/2023-04/graphql.json";
-  if (!event.body) {
-    return {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      statusCode: 400,
-      body: JSON.stringify({ err: "no body :( " }),
-    };
-  }
-  const body = JSON.parse({
+
+  const body = {
     id: 271878346596884000,
     created_at: "2023-12-13T11:49:24+01:00",
     updated_at: "2023-12-13T11:49:24+01:00",
@@ -44,127 +39,183 @@ export const handler: Handler = async (event, context) => {
     harmonized_system_code: null,
     tracked: true,
     country_harmonized_system_codes: [],
-    admin_graphql_api_id: "gid://shopify/InventoryItem/271878346596884015",
-  });
+    admin_graphql_api_id: "gid://shopify/InventoryItem/31709294723183",
+  };
 
   console.log(body);
+  // retreive variand id from InventoryItem id
+  const variantId = await getVariantId(body.admin_graphql_api_id);
+  console.log(variantId);
 
-  return {
+  // retreive all inventory levels of this variant
+  // update status of metafield
+  return getResponse(200, variantId);
+};
+
+function getResponse(
+  code: number,
+  body?: any
+): {
+  headers: { [key: string]: string };
+  statusCode: number;
+  body?: string;
+} {
+  let result = {
+    statusCode: code,
     headers: {
       "Access-Control-Allow-Origin": "*",
     },
-    statusCode: 500,
-    body: JSON.stringify({ err: "No inventory data available" }),
   };
-  const queryBody = `
-  {
-    productVariant(id: "gid://shopify/ProductVariant/${body.variant_id}") {
-      id
-      title
-      inventoryItem {
+  if (body) {
+    return {
+      ...result,
+      body: JSON.stringify(body),
+    };
+  }
+  return result;
+}
+
+interface InventoryLevel {
+  node: {
+    location: {
+      id: string;
+      name: string;
+    };
+    quantities: {
+      quantity: number;
+    }[];
+  };
+}
+interface VariantId {
+  node: {
+    id: string;
+    inventoryItem: {
+      inventoryLevels: {
+        edges: {
+          node: {
+            location: {
+              id: string;
+              name: string;
+            };
+            quantities: {
+              quantity: number;
+            }[];
+          }
+        }[]
+      }
+    }
+  };
+}
+
+interface ParsedData {
+  [variantId: string]: {
+    [locationId: string]: number;
+  };
+}
+
+function parseData(data: any): ParsedData | null {
+  try {
+    if (!data?.data?.inventoryItem?.variant?.inventoryItem?.inventoryLevels?.edges) {
+      throw new Error('Invalid data structure');
+    }
+
+    const result: ParsedData = {};
+    const currentVariantInventoryLevels = data.data.inventoryItem.variant.inventoryItem.inventoryLevels.edges;
+    const currentVariantId = data.data.inventoryItem.variant.id;
+    const variantProductVariants = data.data.inventoryItem.variant.product.variants.edges;
+    
+    function parseInventoryLevels(inventoryLevels: any, variantId: string,) {
+
+      inventoryLevels.forEach((level: InventoryLevel, index: number) => {
+        const locationId = level.node.location.id;
+        const quantity = level.node.quantities.reduce((acc, curr) => acc + curr.quantity, 0);
+  
+        if (!result[variantId]) {
+          result[variantId] = {};
+        }
+  
+        result[variantId][locationId] = quantity;
+      }); 
+    }
+
+    parseInventoryLevels(currentVariantInventoryLevels, currentVariantId);
+
+    variantProductVariants.forEach((variant : VariantId, index : number) => {
+      const variantId = variant.node.id;
+      const inventoryLevels = variant.node.inventoryItem.inventoryLevels.edges;
+      
+      parseInventoryLevels(inventoryLevels, variantId);
+    });
+
+    return result;
+  } catch (error) {
+    console.error('Error parsing data:', error);
+    return null;
+  }
+}
+
+async function getVariantId(inventoryItemId: string) {
+  const queryBody = `{
+    inventoryItem(id: "${inventoryItemId}") {
+      variant {
         id
-        inventoryLevels(first: 10) {
-          edges {
-            node {
-              quantities(names:"available") {
-                name
-                quantity
+        inventoryItem {
+          inventoryLevels(first: 10) {
+            edges {
+              node {
+                location {
+                  id
+                  name
+                }
+                quantities(names: "available") {
+                  quantity
+                }
               }
-              location {
+            }
+          }
+        }
+        product {
+          handle
+          id
+          variants(first: 10) {
+            edges {
+              node {
+                inventoryItem {
+                  inventoryLevels(first: 10) {
+                    edges {
+                      node {
+                        location {
+                          id
+                          name
+                        }
+                        quantities(names: "available") {
+                          quantity
+                        }
+                      }
+                    }
+                  }
+                }
                 id
-                name
               }
             }
           }
         }
       }
     }
-  }
-  `;
+  }`;
 
-  const shopifyData: any = await makeRequest(
-    shopifyGraphEndpoint,
-    "POST",
-    {
-      "Content-Type": "application/json",
-      "X-Shopify-Access-Token": SHOPIFY_ACCESS_TOKEN, // Replace with your actual access token
-    },
-    JSON.stringify({ query: queryBody })
-  );
+  const response = await makeGraphQLRequest(queryBody);
 
-  // Handle the GraphQL response
-  if (!shopifyData || !shopifyData.data || !shopifyData.data.productVariant) {
-    return {
-      statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({
-        err: "Invalid response from Shopify",
-        body,
-        shopifyData,
-      }),
-    };
-  }
+  const parsedData = parseData(response);
 
-  const variant = shopifyData.data.productVariant;
-  const inventoryItem = variant.inventoryItem;
+  return parsedData;
+  // return response;
+  // return response.data.inventoryItem.variant.id;
+}
 
-  if (
-    !inventoryItem ||
-    !inventoryItem.inventoryLevels ||
-    !inventoryItem.inventoryLevels.edges
-  ) {
-    return {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      statusCode: 500,
-      body: JSON.stringify({ err: "No inventory data available" }),
-    };
-  }
-
-  let stockAvailable = null;
-  try {
-    for (let edge of inventoryItem.inventoryLevels.edges) {
-      const node = edge.node;
-      if (node.location && node.location.id === LOCATION_ID) {
-        stockAvailable = node?.quantities?.find(
-          (q) => q.name === "available"
-        )?.quantity;
-        break;
-      }
-    }
-
-    if (stockAvailable === null) {
-      return {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-        },
-        statusCode: 404,
-        body: JSON.stringify({
-          err: "No stock data available for the specified location",
-        }),
-      };
-    }
-
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify({ stock: stockAvailable, body, shopifyData }),
-    };
-  } catch (err) {
-    return {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      statusCode: 500,
-      body: JSON.stringify({ err, body, shopifyData }),
-    };
-  }
-};
+// async function dataParse(response:object) {
+//   console.log(response);
+// }
 
 async function makeGraphQLRequest<T>(
   body: string,
