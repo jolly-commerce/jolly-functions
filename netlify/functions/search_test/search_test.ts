aff1a84a222f27b3d72d4f7049185bc480b9b64b
K0N0HA <107436295+K0N0HATV@users.noreply.github.com>
2024-02-13 17:39:40 +0200

    add secure signature for Shopify app proxy

--- netlify/functions/search_test/search_test.ts
+++ netlify/functions/search_test/search_test.ts
@@ -4,8 +4,11 @@ const { GoogleAuth } = require('google-auth-library');
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
 dotenv.config();
+const querystring = require('querystring')
+const crypto = require('crypto')
 const serviseAccountInfo = JSON.parse(process.env.GOOGLE_CLOUD_INFO)
 const projectId = serviseAccountInfo.project_id
+const searchAppSecretClient = process.env.SEARCH_APP_SECRET_CLIENT
 
 const auth = new GoogleAuth({
   scopes: ['https://www.googleapis.com/auth/cloud_search.query', 'https://www.googleapis.com/auth/cloud_search', 'https://www.googleapis.com/auth/cloud_search.settings.query', 'https://www.googleapis.com/auth/cloud_search.indexing', 'https://www.googleapis.com/auth/cloud_search.debug', 'https://www.googleapis.com/auth/cloud_search.settings', 'https://www.googleapis.com/auth/cloud_search.settings.indexing', 'https://www.googleapis.com/auth/cloud_search.stats', 'https://www.googleapis.com/auth/cloud_search.stats.indexing', "https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/dfatrafficking", "https://www.googleapis.com/auth/ddmconversions", "https://www.googleapis.com/auth/dfareporting",],
@@ -20,22 +23,23 @@ function getGoogleCredentials(private_key, client_email) {
 }
 
 const handler = async (event) => {
-  const checkShop = event?.queryStringParameters?.shop?.includes('myshopify')
-  const eventBody = JSON.parse(event?.body)
-  const querySeaarch = eventBody?.query
+  const originalQuerystring = event.rawQuery
+  const signatureFromClient = querystring.parse(originalQuerystring).signature
+  const computedSignature = computeSignature(originalQuerystring, searchAppSecretClient)
   console.log(event);
-  console.log(checkShop);
-  console.log(querySeaarch);
 
-  if (!checkShop) {
+  if (computedSignature != signatureFromClient) {
     return {
       statusCode: 400,
       body: "not ok"
     };
   }
 
+  const eventBody = event?.body ? JSON.parse(event?.body) : {}
+  const querySeaarch = eventBody?.query
+  console.log(querySeaarch);
   const token = await auth.getAccessToken()
-  const autocompleteResponse = await mainAutocomplete(`https://retail.googleapis.com/v2/projects/${projectId}/locations/global/catalogs/default_catalog`, querySeaarch, token);
+  const autocompleteResponse = await mainAutocomplete(`https://retail.googleapis.com/v2/projects/${projectId}/locations/global/catalogs/default_catalog:completeQuery?query=${querySeaarch}`, token);
   // const searchResponse = mainSearch(`projects/${projectId}/locations/global/catalogs/default_catalog/servingConfigs/default_search`, '123')
   console.log(autocompleteResponse);
 
@@ -43,6 +47,15 @@ const handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(autocompleteResponse)
   };
+}
+
+function computeSignature(querystringFromClient, shopifyAppSecret) {
+  const formattedQueryString = querystringFromClient.replace("/?", "")
+    .replace(/&signature=[^&]*/, "").split("&")
+    .map(x => querystring.unescape(x)).sort().join("")
+  const computedSignature = crypto.createHmac('sha256', shopifyAppSecret)
+    .update(formattedQueryString, 'utf-8').digest('hex')
+  return computedSignature
 }
 
 function mainSearch(placement, visitorId, query) {
@@ -63,28 +76,8 @@ function mainSearch(placement, visitorId, query) {
   callSearch();
 }
 
-async function mainAutocomplete(catalog, query, token) {
-  // v1
-  // const autocompleteProductResponse = await fetch(`https://cloudsearch.googleapis.com/v1/query/suggest`, {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //     catalog,
-  //     query,
-  //     visitorId: '12581147560-qg2973u7ne51adj8uj3mkptqqcot2vvc.apps.googleusercontent.com',
-  //   }),
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'X-Goog-User-Project': projectId,
-  //     'Authorization': `Bearer ${token}`,
-  //   }
-  // })
-  //   .then(res => res.json())
-  //   .catch(err => console.error('error:' + err));
-
-  // return autocompleteProductResponse
-
-  // v2
-  const autocompleteProductResponse = await fetch(`${catalog}:completeQuery?query=${query}`, {
+async function mainAutocomplete(catalog, token) {
+  const autocompleteProductResponse = await fetch(catalog, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -96,20 +89,6 @@ async function mainAutocomplete(catalog, query, token) {
     .catch(err => console.error('error:' + err));
 
   return autocompleteProductResponse
-
-  // example
-  // const retailClient = new CompletionServiceClient();
-
-  // async function callCompleteQuery() {
-  //   const request = {
-  //     catalog,
-  //     query,
-  //   };
-
-  //   return await retailClient.completeQuery(request);
-  // }
-
-  // return callCompleteQuery();
 }
 
 module.exports = { handler }
