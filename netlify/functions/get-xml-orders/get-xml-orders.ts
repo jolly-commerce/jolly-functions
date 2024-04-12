9df61e8e5ceaf56764cb9b4796c47db20743d0a6
Kevin Miguet <kevin@jollycommerce.io>
2024-04-12 12:40:02 +0700

    fis

--- netlify/functions/get-xml-orders/get-xml-orders.ts
+++ netlify/functions/get-xml-orders/get-xml-orders.ts
@@ -1,6 +1,6 @@
 import { Handler } from "@netlify/functions";
 const js2xmlparser = require("js2xmlparser");
-import { data_type } from "./types";
+import { FullfillmentOrder, data_type } from "./types";
 
 // check https://docs.google.com/spreadsheets/d/1Ruaw8xqtg1XsYTtvEPYxbXmF5EiREBs3/edit#gid=1627918533 for more details
 
@@ -15,17 +15,32 @@ export function getObjectWithoutEmptyProperties<T>(object: T): T {
   return result;
 }
 
+function getOrderTotalWeight(fulfillmentOrders: FullfillmentOrder[]) {
+  let total = 0;
+  fulfillmentOrders.forEach((fo) => {
+    fo.lineItems.nodes.forEach((le) => {
+      total += parseFloat(le.weight.value as any as string);
+    });
+  });
+  return total 
+}
+
 export const handler: Handler = async (event, context) => {
   let body: data_type = JSON.parse(event.body);
-
+  const;
   const result = body.map((order) => {
     const billingAddress =
       order.billingAddress != null
         ? order.billingAddress
         : order.shippingAddress;
     return getObjectWithoutEmptyProperties({
-      Codice_Cliente: String(order.customer.id.replace("gid://shopify/Customer/","")).slice(0, -1), // because they want 12 number user ids and cannot change their system. This is the best we can do.
-      Numero_Ordine: String(order.id.replace("gid://shopify/Order/","")).slice(0, -1),
+      Codice_Cliente: String(
+        order.customer.id.replace("gid://shopify/Customer/", "")
+      ).slice(0, -1), // because they want 12 number user ids and cannot change their system. This is the best we can do.
+      Numero_Ordine: String(order.id.replace("gid://shopify/Order/", "")).slice(
+        0,
+        -1
+      ),
       Ragione_Sociale_Destinatario: `${order.shippingAddress.firstName} ${order.shippingAddress.lastName}`,
       Indirizzo_Destinatario: order.shippingAddress.address1,
       Localita_Destinatario: order.shippingAddress.city,
@@ -40,10 +55,14 @@ export const handler: Handler = async (event, context) => {
       CAP_Destinazione_Merce: billingAddress.zip,
       Provincia_Destinazione_Merce: billingAddress.provinceCode,
       Nazione_Destinazione_Merce: billingAddress.countryCodeV2,
-      Codice_Vettore: order.fulfillments[0],
+      Codice_Vettore: "default",  // order.shippingLines.nodes[0].title
+      Peso_Carico_Previsto: getOrderTotalWeight(order.fulfillmentOrders.nodes),
+      Aspetto1_Qta: order.shippingLines.nodes.length,
       Righe_Ordine: {
         Riga_Ordine: order.lineItems.nodes.map((line_item, k) => ({
-          Codice_Cliente: String(order.customer.id.replace("gid://shopify/Customer/","")).slice(0, -1),
+          Codice_Cliente: String(
+            order.customer.id.replace("gid://shopify/Customer/", "")
+          ).slice(0, -1),
           Numero_Ordine: `0000${order.name.replace("#", "")}`,
           Numero_Riga: k + 1,
           Numero_SottoRiga: 1,
